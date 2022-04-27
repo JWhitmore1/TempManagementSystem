f86f29e9dd26247a62490dd5ddd9755efdb295bb
JWhitmore1 <38652089+JWhitmore1@users.noreply.github.com>
2022-04-28 08:06:55 +1000

    update server.py

--- DataloggerServer/app.py
+++ DataloggerServer/app.py
@@ -1,15 +1,18 @@
 from flask import Flask, redirect
 import serial
 import json
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    ser = serial.Serial('/dev/cu.usbmodem11301', 9600)
+    # running on raspberry pi
+    ser = serial.Serial('/dev/ttyACM0', 9600)
     data = ser.readline().decode("utf-8")
     try:
         arduino_json = json.loads(data)
+        arduino_json['data_date_time'] = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
     except:
         return redirect("/")
     return arduino_json
